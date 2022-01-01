2b3f3339607b53d209ad0dca8b9bee8c9b34e946
kang <dkemfjqek@gmail.com>
2022-01-01 22:57:45 +0900

    commit

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,49 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        //code
+//      Member member = new Member();
+//
+//      member.setId(2L);
+//      member.setName("kangB");
+
+        //em.persist(member);
+        try {
+//            수정
+//            Member findMember = em.find(Member.class, 1L);
+//            System.out.println("findMember.Id >>" + findMember.getId());
+//            System.out.println("findMember.name >>" + findMember.getName());
+//
+//            findMember.setName("kangA");
+
+            //JPQL
+            List<Member> resultList = em.createQuery("select m from Member as m", Member.class).getResultList();
+
+            for (Member member : resultList) {
+                System.out.println("JpaMain.main member.name =" + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
